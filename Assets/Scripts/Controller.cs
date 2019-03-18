72f73e6725bd54496166cd9d47c13c7f744c1327
shadowegemon <shadowegemon@gmail.com>
2019-03-18 13:43:05 +0100

    First

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 moveVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = move.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + moveVelocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject borderPrefab;
+
+    [SerializeField]
+    private int tileSize;
+
+    private List<GameObject> tiles = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateBorder(10, 10);
+    }
+
+    private void GenerateBorder(int x, int y)
+    {
+        for (int i = 0; i < x; ++i)
+        {
+            for (int j = 0; j < x; ++j)
+            {
+                if (i == 0 || i == x - 1 || j == 0 || j == y - 1)
+                {
+                    GameObject tile = Instantiate<GameObject>(borderPrefab);
+
+                    tile.transform.position = new Vector3(tileSize * i, tileSize * j, 0);
+                    tiles.Add(tile);
+                }
+            }
+        }
+
+
+    }
+}
